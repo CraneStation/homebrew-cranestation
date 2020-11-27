2539863e5f26d12c2572a47e9aef7fe927f933c8
Hana Dusíková <hana.dusikova@avast.com>
2020-11-27 11:04:48 +0100

    update URL and remove SHA256 checksum as the 'dev' changes often

--- Formula/wasmtime.rb
+++ Formula/wasmtime.rb
@@ -1,11 +1,9 @@
 class Wasmtime < Formula
   desc "Standalone JIT-style runtime for WebAssembly, using Cranelift"
   homepage "https://github.com/cranestation/wasmtime"
-  url "https://github.com/CraneStation/wasmtime/releases/download/dev/wasmtime-dev-x86_64-apple.tar.xz"
-  sha256 "5df78b998aa9aa7d50bc66f719bef4cfc4767d7e44ddd89f6658ef3d847204eb"
+  url "https://github.com/bytecodealliance/wasmtime/releases/download/dev/wasmtime-dev-x86_64-macos.tar.xz"
 
   def install
     bin.install 'wasmtime'
-    bin.install 'wasm2obj'
   end
 end
